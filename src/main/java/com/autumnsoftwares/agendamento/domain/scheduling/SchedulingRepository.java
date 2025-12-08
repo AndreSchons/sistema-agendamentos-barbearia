b6910dad513c8f3c437af65a25cff4096b97833a
Andre Schons <andreferens86@gmail.com>
2025-12-08 14:25:56 -0300

    Status mudando apos o endTime

--- src/main/java/com/autumnsoftwares/agendamento/domain/scheduling/SchedulingRepository.java
+++ src/main/java/com/autumnsoftwares/agendamento/domain/scheduling/SchedulingRepository.java
@@ -1,8 +1,15 @@
 package com.autumnsoftwares.agendamento.domain.scheduling;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SchedulingRepository extends JpaRepository<Scheduling, Integer> {
+
+    @Query("SELECT s FROM Scheduling s WHERE s.endTime < :now AND s.status = 'SCHEDULED'")
+    List<Scheduling> findAllPastAndScheduled(LocalDateTime now);
 }
--- src/main/java/com/autumnsoftwares/agendamento/domain/scheduling/SchedulingService.java
+++ src/main/java/com/autumnsoftwares/agendamento/domain/scheduling/SchedulingService.java
@@ -1,7 +1,12 @@
 package com.autumnsoftwares.agendamento.domain.scheduling;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import com.autumnsoftwares.agendamento.domain.barber.Barber;
 import com.autumnsoftwares.agendamento.domain.barber.BarberRepository;
@@ -17,6 +22,8 @@ import jakarta.transaction.Transactional;
 
 @Service
 public class SchedulingService {
+
+    private static final Logger log = LoggerFactory.getLogger(SchedulingService.class);
 
     private final SchedulingRepository schedulingRepository;
     private final SchedulingMapper schedulingMapper;
@@ -59,5 +66,20 @@ public class SchedulingService {
         return schedulingRepository.findById(id).map(schedulingMapper::toResponseDTO);
     }
 
+    @Scheduled(fixedRate = 60000) // Roda a cada 60000 ms = 1 minuto
+    @Transactional
+    public void updateStatusForCompletedSchedules() {
+        log.info("Running scheduled task to update completed schedules...");
+        List<Scheduling> pastSchedules = schedulingRepository.findAllPastAndScheduled(LocalDateTime.now());
+
+        if (pastSchedules.isEmpty()) {
+            log.info("No schedules to update.");
+            return;
+        }
+
+        pastSchedules.forEach(schedule -> schedule.setStatus(SchedulingStatus.COMPLETED));
+        schedulingRepository.saveAll(pastSchedules);
+        log.info("Updated {} schedules to COMPLETED status.", pastSchedules.size());
+    }
     
 }
