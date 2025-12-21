298c5234cb38f0b950c933353f5bca34b4de758c
Andre Schons <andreferens86@gmail.com>
2025-12-21 20:58:51 -0300

    Documentacao ServiceTypeController com swagger

--- src/main/java/com/autumnsoftwares/agendamento/domain/services_type/ServiceTypeController.java
+++ src/main/java/com/autumnsoftwares/agendamento/domain/services_type/ServiceTypeController.java
@@ -2,6 +2,12 @@ package com.autumnsoftwares.agendamento.domain.services_type;
 
 import com.autumnsoftwares.agendamento.domain.services_type.dto.ServiceTypeCreateRequestDTO;
 import com.autumnsoftwares.agendamento.domain.services_type.dto.ServiceTypeResponseDTO;
+import com.autumnsoftwares.agendamento.infra.security.SecurityConfigurations;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/services")
+@Tag(name = "service type controller")
 public class ServiceTypeController {
 
     private final ServiceTypeService serviceTypeService;
@@ -19,6 +26,11 @@ public class ServiceTypeController {
         this.serviceTypeService = serviceTypeService;
     }
 
+    @Operation(summary = "Criar novo tipo de serviço", description = "Metodo para criar um novo tipo de serviço (Requer ADMIN)")
+    @ApiResponse(responseCode = "201", description = "Serviço criado com sucesso")
+    @ApiResponse(responseCode = "403", description = "Acesso negado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
+    @SecurityRequirement(name = SecurityConfigurations.SECURITY)
     @PostMapping
     public ResponseEntity<ServiceTypeResponseDTO> createService(@RequestBody @Valid ServiceTypeCreateRequestDTO createRequestDTO, UriComponentsBuilder uriComponentsBuilder) {
         ServiceTypeResponseDTO createdService = serviceTypeService.createService(createRequestDTO);
@@ -28,11 +40,20 @@ public class ServiceTypeController {
         return ResponseEntity.created(uri).body(createdService);
     }
 
+    @Operation(summary = "Listar todos os tipos de serviço", description = "Metodo para listar todos os serviços disponíveis")
+    @ApiResponse(responseCode = "200", description = "Listagem realizada com sucesso")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
+    @SecurityRequirement(name = SecurityConfigurations.SECURITY)
     @GetMapping
     public ResponseEntity<List<ServiceTypeResponseDTO>> getAllServices() {
         return ResponseEntity.ok(serviceTypeService.getAllServices());
     }
 
+    @Operation(summary = "Deletar tipo de serviço", description = "Metodo para deletar um tipo de serviço por ID")
+    @ApiResponse(responseCode = "204", description = "Serviço deletado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Serviço não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
+    @SecurityRequirement(name = SecurityConfigurations.SECURITY)
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteServiceType(@PathVariable Integer id) {
         serviceTypeService.deleteById(id);
