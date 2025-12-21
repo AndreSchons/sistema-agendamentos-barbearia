55351b31b0098f431d9ae92693767677e6d6c17c
Andre Schons <andreferens86@gmail.com>
2025-12-21 20:58:33 -0300

    Documentacao SchedulingController com swagger

--- src/main/java/com/autumnsoftwares/agendamento/domain/scheduling/SchedulingController.java
+++ src/main/java/com/autumnsoftwares/agendamento/domain/scheduling/SchedulingController.java
@@ -16,10 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import com.autumnsoftwares.agendamento.domain.scheduling.dto.SchedulingCreateRequestDTO;
 import com.autumnsoftwares.agendamento.domain.scheduling.dto.SchedulingResponseDTO;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/scheduling")
+@Tag(name = "scheduling controller")
 public class SchedulingController {
     
     private final SchedulingService schedulingService;
@@ -30,6 +35,10 @@ public class SchedulingController {
         this.availableSlotsService = availableSlotsService;
     }
 
+    @Operation(summary = "Agendar um serviço", description = "Metodo para criar um novo agendamento")
+    @ApiResponse(responseCode = "201", description = "Agendamento criado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Cliente, Barbeiro ou Serviço não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     @PostMapping
     public ResponseEntity<SchedulingResponseDTO> createScheduling(@RequestBody @Valid SchedulingCreateRequestDTO schedulingCreateRequestDTO, UriComponentsBuilder uriComponentsBuilder) {
         SchedulingResponseDTO createdScheduling = schedulingService.createScheduling(schedulingCreateRequestDTO);
@@ -39,12 +48,20 @@ public class SchedulingController {
         return ResponseEntity.created(uri).body(createdScheduling);
     }
 
+    @Operation(summary = "Cancelar agendamento", description = "Metodo para cancelar um agendamento existente")
+    @ApiResponse(responseCode = "200", description = "Agendamento cancelado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Agendamento não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     @PostMapping("/cancell")
     public ResponseEntity<Void> cancellScheduling(@RequestParam Integer schedulingId) {
         schedulingService.cancellScheduling(schedulingId);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Buscar agendamento por ID", description = "Metodo para buscar detalhes de um agendamento")
+    @ApiResponse(responseCode = "200", description = "Agendamento encontrado")
+    @ApiResponse(responseCode = "404", description = "Agendamento não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     @GetMapping("/{id}")
     public ResponseEntity<SchedulingResponseDTO> getSchedulingById(@PathVariable Integer id) {
         return schedulingService.getSchedulingById(id)
@@ -52,6 +69,10 @@ public class SchedulingController {
             .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Listar horários disponíveis", description = "Metodo para listar horários disponíveis para um barbeiro e serviço em uma data específica")
+    @ApiResponse(responseCode = "200", description = "Horários disponíveis listados com sucesso")
+    @ApiResponse(responseCode = "404", description = "Barbeiro ou Serviço não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     @GetMapping("/available-slots")
     public ResponseEntity<List<LocalTime>> getAvailableSlots(@RequestParam Integer barberId, @RequestParam Integer serviceTypeId, @RequestParam LocalDate localDate) {
         List<LocalTime> availableSlots = availableSlotsService.getAvailableSlots(barberId, serviceTypeId, localDate);
