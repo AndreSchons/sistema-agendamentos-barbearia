79259dacc39503ee0c7489a3d77da7eae27c4e28
Andre Schons <andreferens86@gmail.com>
2025-12-21 20:58:12 -0300

    Documentacao CustomerController com swagger

--- src/main/java/com/autumnsoftwares/agendamento/domain/customer/CustomerController.java
+++ src/main/java/com/autumnsoftwares/agendamento/domain/customer/CustomerController.java
@@ -4,6 +4,11 @@ import java.net.URI;
 import java.util.List;
 
 import com.autumnsoftwares.agendamento.domain.scheduling.dto.SchedulingResponseDTO;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +28,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/customer")
+@Tag(name = "customer controller")
 public class CustomerController {
     
     private final CustomerService customerService;
@@ -31,6 +37,10 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @Operation(summary = "Salva os dados de um cliente", description = "Metodo para salvar dados do cliente")
+    @ApiResponse(responseCode = "201", description = "Cliente salvo com sucesso")
+    @ApiResponse(responseCode = "400", description = "Dados invalidos")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     @PostMapping
     public ResponseEntity<CustomerResponseDTO> createCustomer(@RequestBody @Valid CustomerCreateRequestDTO customerCreateRequestDTO, UriComponentsBuilder uriComponentsBuilder) {
         CustomerResponseDTO createdCustomer = customerService.createCustomer(customerCreateRequestDTO);
@@ -40,24 +50,39 @@ public class CustomerController {
         return ResponseEntity.created(uri).body(createdCustomer);
     }
 
+    @Operation(summary = "Pega um cliente pelo numero de telefone", description = "Metodo para pegar cliente por telefone")
+    @ApiResponse(responseCode = "200", description = "Cliente encontrado")
+    @ApiResponse(responseCode = "404", description = "Cliente nao encontrado!")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     @GetMapping("/search")
     public ResponseEntity<CustomerResponseDTO> getCustomerByPhone(@RequestParam("phone") String phone) {
         return customerService.getCustomerByPhone(phone)
         .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Deletar cliente por ID", description = "Metodo para deletar o cliente via ID")
+    @ApiResponse(responseCode = "204", description = "Cliente deletado com sucesso!")
+    @ApiResponse(responseCode = "404", description = "Cliente nao encontrado!")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCustomer(@PathVariable Integer id) {
         customerService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Listar todos os clientes", description = "Metodo para listar todos os clientes")
+    @ApiResponse(responseCode = "200", description = "Listagem realizada com sucesso")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     @GetMapping
     public ResponseEntity<List<CustomerResponseDTO>> getAllCustomers() {
         List<CustomerResponseDTO> customers = customerService.findAll();
         return ResponseEntity.ok(customers);
     }
 
+    @Operation(summary = "Pegar cliente por ID", description = "Metodo para pegar os dados do cliente via ID")
+    @ApiResponse(responseCode = "200", description = "Cliente encontrado com sucesso!")
+    @ApiResponse(responseCode = "404", description = "Cliente nao encontrado!")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     @GetMapping("/{id}")
     public ResponseEntity<CustomerResponseDTO> getCustomerById(@PathVariable Integer id) {
         return customerService.findById(id)
@@ -65,12 +90,20 @@ public class CustomerController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Atualizar cliente por ID", description = "Metodo para atualizar os dados do cliente via ID")
+    @ApiResponse(responseCode = "200", description = "Cliente atualizado com sucesso!")
+    @ApiResponse(responseCode = "404", description = "Cliente nao encontrado!")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     @PutMapping("/{id}")
     public ResponseEntity<CustomerResponseDTO> updateCustomer(@PathVariable Integer id, @RequestBody @Valid CustomerUpdateRequestDTO updateRequestDTO) {
         CustomerResponseDTO updatedCustomer = customerService.updateById(id, updateRequestDTO);
         return ResponseEntity.ok(updatedCustomer);
     }
 
+    @Operation(summary = "Listar agendamentos do cliente", description = "Metodo para listar agendamentos buscando pelo telefone do cliente")
+    @ApiResponse(responseCode = "200", description = "Agendamentos encontrados")
+    @ApiResponse(responseCode = "404", description = "Cliente nao encontrado!")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     @GetMapping("/search/schedulings")
     public ResponseEntity<List<SchedulingResponseDTO>> getSchedulingsByPhone(@RequestParam("phone") String phone) {
         List<SchedulingResponseDTO> schedulings = customerService.getSchedulingsByPhone(phone);
